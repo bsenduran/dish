e117a33f084eb5cc74eea5d4860b79280949c296
Buddhima <buddhimawijeweera@yahoo.com>
2015-06-19 11:46:54 +0530

    Adding forever triggering

--- themes/default/js/update-name.js
+++ themes/default/js/update-name.js
@@ -16,4 +16,20 @@ $(function(){
         });
     });
 
+    // Toggling trigger-count based on checkbox status
+    $('#triggerForever').on('click',function(){
+        var checkedState = document.getElementById("triggerForever").checked;
+        var textBox = document.getElementById("triggerCount");
+
+        if(checkedState) {
+            textBox.disabled = true;
+            textBox.value = -1;
+            textBox.style.visibility = "hidden";
+        } else {
+            textBox.disabled = false;
+            textBox.value = "";
+            textBox.style.visibility = "visible";
+        }
+    });
+
 });
